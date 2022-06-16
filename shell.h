4ce2e27bff0bdf782658465c9888cc126638639d
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-06-16 12:49:49 +0100

    Addd _strcat to sring functions

--- shell.h
+++ shell.h
@@ -34,6 +34,7 @@ int _strlen(char *s);
 int _strncmp(char *s1, char *s2, int n);
 char *_strdup(char *s);
 char *_strchr(char *s, char c);
+char *_strcat(char *dest, const char *src);
 
 void execution(char *cp, char **cmd);
 char *find_path(void);
--- string_functions.c
+++ string_functions.c
@@ -103,3 +103,27 @@ char *_strchr(char *s, char c)
 		return (s);
 	return (NULL);
 }
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: char pointer to the dest of the copied str
+ * @src: char pointer to the source str
+ * Return: The dest str
+ */
+
+char *_strcat(char *dest, const char *src)
+
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+                ;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
